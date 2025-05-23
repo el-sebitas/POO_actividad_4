cbd1ced038f7cebcd470d4073ec6c6cc099ff426
sebastianvg06 <sebsv16@gmail.com>
2025-05-22 19:41:57 -0500

    Subida de archivos

---
+++ src/Notas.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Notas {
+    private ArrayList<Double> notas = new ArrayList<>();
+
+    public void ingresarNota(double nota) {
+        notas.add(nota);
+    }
+
+    public double promedio() {
+        double total = 0;
+        for (double nota: notas) {
+            total += nota;
+        }
+        return total / notas.size();
+    }
+
+    public double mayorNota() {
+        ArrayList<Double> notas = this.notas;
+        Collections.sort(notas);
+        return notas.get(notas.size() - 1);
+    }
+
+    public double menorNota() {
+        ArrayList<Double> notas = this.notas;
+        Collections.sort(notas);
+        return notas.get(0);
+    }
+
+    public double desviacion() {
+        double prom = promedio();
+        double suma = 0;
+
+        for (double nota: notas) {
+            suma = Math.pow(nota, 2);
+        }
+        return Math.sqrt(suma);
+    }
+}
